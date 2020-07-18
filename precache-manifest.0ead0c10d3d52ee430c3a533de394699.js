ac1c1801a60a43ad35d18c3a2ffd0460c60bd9b5
Golu <anurag.bhardwaj.cpp@gmail.com>
2020-07-18 13:35:46 +0530

    Updates

---
+++ precache-manifest.0ead0c10d3d52ee430c3a533de394699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7faaef8beb0e13fc66ea958ebe597d23",
+    "url": "/himachal/index.html"
+  },
+  {
+    "revision": "e96328f787c1b8c4c4dc",
+    "url": "/himachal/static/css/main.888f9038.chunk.css"
+  },
+  {
+    "revision": "58d5bde1db4b9d7609c8",
+    "url": "/himachal/static/js/2.0fd2cd01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/himachal/static/js/2.0fd2cd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96328f787c1b8c4c4dc",
+    "url": "/himachal/static/js/main.5ea86954.chunk.js"
+  },
+  {
+    "revision": "fd8e508a9e9af78edb1c",
+    "url": "/himachal/static/js/runtime-main.46657b57.js"
+  }
+]);
